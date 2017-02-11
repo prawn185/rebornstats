48421435b28a6ec8e117405a7a04755d5a83bee9
Shaun Moore <shaun.moore@netmatters.com>
2017-02-11 10:00:25 +0000

    Fixing foreach and adding styles to the headers

--- global/global.php
+++ global/global.php
@@ -4,6 +4,7 @@ include('database.php');
 ?>
 
 <head>
+
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -12,6 +13,11 @@ include('database.php');
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <style>
+        <?php include_once('player.css'); ?>
+    </style>
+
 
     <title><?php echo $pagetitle ?></title>
 
--- index.php
+++ index.php
@@ -1,56 +1,47 @@
 <?php
-include('global.php')
+//$pagetitle = "Database";
+include('global/global.php');
+$query = "select * from players";
+$result = $conn->query($query);
+
+    $row = $result->fetch_all();
+
+
+
+    ?>
+    <h3>Citizen Database</h3>
+    <?php
+    /* fetch object array */ ?>
+
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="">
+                <div class="civ-head">
+                    <h3 class="panel-title">:
+                        Civilians
+                    </h3>
+                </div>
+
+            </div>
+            <?php
+
+            foreach($row as $rows) {
+                ?>
+
+                <div class="panel-body">
+                    <div class="item-player">
+                        <div class="col-md-4"><?php echo $rows[1]; ?></div>
+                        <div class="col-md-4"><?php echo $rows[4]; ?></div>
+                        <div class="col-md-4"><?php echo $rows[5]; ?></div>
+                    </div>
+
+                </div>
+                <?php
+            }
+            ?>
+
+        </div>
+    </div>
+    <?php
+
 ?>
-
-
-<div class="row">
-    <form method="post" action="form-handler.php">
-        <div class="col-md-6 ">
-            <div class="form-group">
-                <label for="customer_name">Customer Name</label>
-                <select class="form-control" name="customer_name" id="customer_name">
-                    <option value="Rent4Sure">Rent4Sure</option>
-                    <option value="Tescos">Tescos</option>
-                    <option value="Sainsburys">Sainsburys</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="line_1">Property Name</label>
-                <input class="form-control" type="text" name="name" placeholder="Dove House">
-            </div>
-            <div class="form-group">
-                <label for="line_1">Address Line 1</label>
-                <input class="form-control" type="text" name="line_1" placeholder="Address Line 1">
-            </div>
-            <div class="form-group">
-                <label for="line_2">Address Line 2</label>
-                <input class="form-control" type="text" name="line_2" placeholder="Address Line 2">
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="country">Country</label>
-                <input class="form-control" type="text" name="country" placeholder="Country">
-            </div>
-            <div class="form-group">
-                <label for="county">County</label>
-                <input class="form-control" type="text" name="county" placeholder="County">
-            </div>
-
-            <div class="form-group">
-                <label for="postcode">Postcode</label>
-                <input class="form-control" type="text" name="postcode" placeholder="Postcode">
-            </div>
-            <div class="form-group">
-                <label for="postcode">Monthly Price</label>
-                <input class="form-control" type="number" name="rent" placeholder="0.00">
-            </div>
-        </div>
-        <div class="col-md-4 col-md-offset-4">
-            <input class="form-control btn btn-success" type="submit" value="Submit">
-        </div>
-    </form>
-</div>
-
-</body>
-</html>
